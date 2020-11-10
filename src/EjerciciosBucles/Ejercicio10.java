8076d9ce5af6d5fa19d728d4a4cab3a9409b9e7a
lluc <mataslluc@gmail.com>
2020-11-10 09:10:41 +0100

    Ejercicios

--- src/EjerciciosBucles/Ejercicio10.java
+++ src/EjerciciosBucles/Ejercicio10.java
@@ -4,19 +4,22 @@ import java.util.Scanner;
 A priori no sabem quant de nombres hi haurà, però sabem que l’usuari indicarà que no vol inserir més nombres quan introdueixi un nombre negatiu. */
 public class Ejercicio10 {
     public static void main(String[] args) {
+        //Invocamos scanner
         Scanner sc = new Scanner(System.in);
+        //Declaracion de variables
         boolean respuesta = false;
         double numGuardado = 0;
         int contador = 0;
             do {
+                //Recogemos datos por teclado
                 System.out.println("Introduzca un numero ");
                 double numSc = sc.nextDouble();
-
+                //Si el numero recodigo por teclado es mas grande que cero:
                 if (numSc < 0)
-                    respuesta = true;
-                    numGuardado += numSc;
-                    contador++;
-            }while (!respuesta);{
+                    respuesta = true; //Cambiar valor de variable booleana a true
+                    numGuardado += numSc; //Sumar e igualar la variable numGuardado + numSc
+                    contador++; //Aumentar el contador en 1
+            }while (!respuesta);{ //cuando respuesta sea false, printar
                 System.out.println("La media es " + numGuardado / contador);
         }
     }
