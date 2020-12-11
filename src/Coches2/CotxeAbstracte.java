18c614d532414faa9347682c71766c9130bcb356
lluc <mataslluc@gmail.com>
2020-12-11 17:32:48 +0100

    Coches 2

---
+++ src/Coches2/CotxeAbstracte.java
@@ -0,0 +1,27 @@
+package Coches2;/*
+  Ejercicios
+  
+  Created by: Coches
+
+  GitHub: https://github.com/LMatass
+    
+  Date: 4/12/20
+  
+  Hora inicio: 17:34
+  
+  Descripción:  
+
+*/
+
+public abstract class CotxeAbstracte implements InterfaceCotxe {
+
+        protected String marca;
+        protected String model;
+        protected TipusCanvi tipuscanvi;
+        public CotxeAbstracte(String marca, String model, TipusCanvi tipuscanvi) {
+            this.marca=marca;
+            this.model=model;
+            this.tipuscanvi=tipuscanvi;
+        }
+
+    }
